835b4ec11cf71899aaaf77d500998e2dd0f0d116
Jordan Paul <jordan@getpixeltag.com>
2024-10-20 06:57:59 -0700

    taking off fund button

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <Stack direction={'column'} spacing={2} alignItems={'center'} justifyContent={'center'} minHeight={'50vh'}> 
       <Typography variant="h3" textAlign={'center'}>Xucre Index Funds</Typography>
-      <OnRamp />
+   
     </Stack>
   );
 }
